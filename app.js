37ffe3dbb1f953af39501b57e2f070e4e4b92ed3
Pelle Lundgren <pellburt@gmail.com>
2024-07-09 07:33:21 +0200

    Add comments

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-// Immediately-invoked function as entry point
+/**
+ * Immediately-invoked function as an entry point.
+ * The entry point job is to start the execution and have access to command line arguments
+ *
+ * Initialization happens within the try/catch block.
+ * Take care of all necessary steps for the service to run such as,
+ * start the web server, load configuration, establish database connection, and so on.
+ */
 (async () => {
   try {
-    // ...
+    // Initialization
   } catch (error) {
     console.log(error);
     // eslint-disable-next-line no-undef
