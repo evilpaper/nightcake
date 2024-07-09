d5a77c5a72ec81bfcf4bb808fc40f47d39372e8b
Pelle Lundgren <pellburt@gmail.com>
2024-07-09 07:26:10 +0200

    Add entry point

---
+++ app.js
@@ -0,0 +1,10 @@
+// Immediately-invoked function as entry point
+(async () => {
+  try {
+    // ...
+  } catch (error) {
+    console.log(error);
+    // eslint-disable-next-line no-undef
+    process.exit(1);
+  }
+})();
