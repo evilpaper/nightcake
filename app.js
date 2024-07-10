749db215eb0fb7213f3e96b3340cf5d1e187ac00
Pelle Lundgren <pellburt@gmail.com>
2024-07-10 18:10:24 +0200

    Add server loader module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const server = require("./loaders/server");
 /**
  * Immediately-invoked function as an entry point.
  * The entry point job is to start the execution and have access to command line arguments
@@ -6,9 +7,13 @@
  * Take care of all necessary steps for the service to run such as,
  * start the web server, load configuration, establish database connection, and so on.
  */
+
 (async () => {
   try {
     // Initialization
+    server.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
   } catch (error) {
     console.log(error);
     // eslint-disable-next-line no-undef
