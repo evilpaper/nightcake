b56a9d56b0555717f0555f44da0d0b836fa7b018
Pelle Lundgren <pellburt@gmail.com>
2024-07-13 16:10:20 +0200

    Implement fallback route

--- loaders/server.js
+++ loaders/server.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const healthController = require("../controllers/health");
+const fallbackController = require("../controllers/fallback");
 
 module.exports = (env) => {
   const app = express();
 
   app.get("/health", healthController);
+  app.all("*", fallbackController);
 
   return app;
 };
