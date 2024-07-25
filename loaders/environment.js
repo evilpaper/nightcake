bfa8bda0c1024ce68e16766894b5d862daff738c
Pelle Lundgren <pellburt@gmail.com>
2024-07-25 07:47:41 +0200

    Setup database configuration, install packages, add env loader

--- loaders/environment.js
+++ loaders/environment.js
@@ -17,5 +17,15 @@ module.exports = () => {
     jwt: {
       secret: env.parsed.JWT_SECRET,
     },
+    database: {
+      name: env.parsed.DATABASE_NAME,
+      user: env.parsed.DATABASE_USER,
+      password: env.parsed.DATABASE_PASSWORD,
+      host: env.parsed.DATABASE_HOST,
+      port: parseInt(env.parsed.DATABASE_PORT, 10),
+      dialect: env.parsed.DATABASE_DIALECT,
+      logging: env.parsed.DATABASE_LOGGING === "TRUE",
+      sync: env.parsed.DATABASE_SYNC === "TRUE",
+    },
   };
 };
