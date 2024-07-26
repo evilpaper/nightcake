ac73ebb6bd17fb36a1af82f433434e266f0c817a
Pelle Lundgren <pellburt@gmail.com>
2024-07-26 08:24:26 +0200

    Initialize the repositories

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 /* eslint-disable no-unused-vars */
-/**
- * We "load" the stuff we need.
- */
 const Env = require("./loaders/environment");
 const Database = require("./loaders/database");
 const Server = require("./loaders/server");
+const Models = require("./models");
+const Repositories = require("./repositories");
 /**
- * We use an immediately-invoked function as an entry point.
- * This give us access to command line arguments.
+ * Immediately-invoked function is used as an entry point to give access to command line arguments.
  */
 (async () => {
   /**
-   * We use a try/catch block to initialize the application.
-   * This so we can exit gracefully in case the process fails.
+   * try/catch block is used to initialize the application so we can exit gracefully in case the process fails.
+   *
+   * dependecy injection pattern is used to inject the configuration (env module).
+   * This gives use more loose coupling. In case we want to change the env module we only need to
+   * do it in the env module. As long as the interface is the same the rest of our application
+   * will be fine.
    */
   try {
     const env = Env();
     const db = await Database(env);
-    /**
-     * Why use dependecy injection pattern to inject the configuration (env module).
-     * This gives use more loose coupling. In case we want to change the env module we only need to
-     * do it in the env module. As long as the interface is the same the rest of our application
-     * will be fine.
-     */
+    const models = await Models(db, env);
+    const repos = await Repositories(models);
     const server = Server(env);
     server.listen(env.server.port, () => {
       console.log(`Server is running on port ${env.server.port}`);
