a6379f00e495f4c06744d6468561dd0dfc4325fd
Pelle Lundgren <pellburt@gmail.com>
2024-08-21 20:46:08 +0200

    Clarify use of force when syncing database

--- models/user.js
+++ models/user.js
@@ -19,6 +19,11 @@ module.exports = async (db, env) => {
     },
   });
 
+  /**
+   * force: false creates the table if it doesn't exist (and does nothing if it already exists)
+   * force: true creates the table, dropping it first if it already existed
+   */
+
   await UserModel.sync({ force: env.database.sync });
 
   return UserModel;
